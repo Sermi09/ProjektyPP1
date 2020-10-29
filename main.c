8986c446eb35d6728cc698ac7df575900eff7588
Sermi09 <72806396+Sermi09@users.noreply.github.com>
2020-10-29 17:17:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+int main()
+{
+  int tab1[5]={1,2,3,4,5},tab2[5];
+  for(int i=0,j=4;i<5,j>=0;++i,--j)
+  {
+    tab2[i]=tab1[j];
+  }
+  for(int i=0;i<5;++i)
+  {
+  printf("%d\n", tab2[i]);
+  }
+
+  return 0;
+}
+
+----------------------------------------------
+
+#include <stdio.h>
+
 teskt (char a[])
 {
     static int b=1;
