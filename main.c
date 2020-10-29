eef869cbde5a78e28b9ded6d50d2d7cb3216c0e6
Sermi09 <72806396+Sermi09@users.noreply.github.com>
2020-10-29 17:19:51 +0100

    Update main.c

--- main.c
+++ main.c
@@ -14,26 +14,3 @@ int main()
 
   return 0;
 }
-
-----------------------------------------------
-
-#include <stdio.h>
-
-teskt (char a[])
-{
-    static int b=1;
-    printf("%d %s\n", b , a);
-    ++b;
-}
-
-int main()
-{
-    int a[255];
-    printf("Podaj tekst\n");
-    scanf("%s", a);
-    teskt (a);
-    teskt (a);
-    teskt (a);
-
-    return 0;
-}
